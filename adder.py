d35e5eafa2ce440979d74d277789287c6062f7fa
Darius Bacon <darius@wry.me>
2015-05-31 21:25:49 -0500

    factor out conversions between uints and envs

--- adder.py
+++ adder.py
@@ -5,27 +5,33 @@ Check some adder implementations.
 import bdd
 
 ## test_adder(4, ripple_carry_add)
+#. 'passed'
 
 def test_adder(n, add):
     "Make an n-bit adder and test it exhaustively."
-    c_in, = map(bdd.Variable, range(-1, 0))
+    c_in = bdd.Variable(-1)
     A = map(bdd.Variable, range(0, n))
     B = map(bdd.Variable, range(n, 2*n))
     c_out, Sum = add(A, B, c_in) # XXX confusable name
     for av in range(2**n):
         for bv in range(2**n):
             for civ in 0, 1:
-                env = {}
-                env[-1] = civ
-                for p in range(n):
-                    env[p]   = (av >> p) & 1
-                    env[n+p] = (bv >> p) & 1
+                env = {c_in.rank: civ}
+                env.update(env_from_uint(A, av))
+                env.update(env_from_uint(B, bv))
                 cov = c_out.evaluate(env)
-                sv = sum(sp.evaluate(env) << p for p, sp in enumerate(Sum))
+                sv = uint_from_bdds(Sum, env)
                 assert (cov << n) + sv == av + bv + civ
+    return 'passed'
     # TODO: symbolically test equivalence with a fancier adder circuit
-    #       nicer code above, making it easier to vary the variable ranking
     #       pick a better ranking of variables (interleaved)
+    #       rename .rank to .var?
+
+def env_from_uint(bit_nodes, value):
+    return {node.rank: (value >> p) & 1 for p, node in enumerate(bit_nodes)}
+
+def uint_from_bdds(bit_nodes, env):
+    return sum(node.evaluate(env) << p for p, node in enumerate(bit_nodes))
 
 def ripple_carry_add(A, B, carry):
     "The simplest adder in logic gates."
